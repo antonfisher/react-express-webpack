71ba7dceebbee727a1f3125e2b266e3a5ab89141
Anton Fisher <a.fschr@gmail.com>
2017-03-26 01:01:38 -0700

    Update deps

--- src/client/logger.js
+++ src/client/logger.js
@@ -1,10 +1,10 @@
 import {Iterable} from 'immutable';
-import createLogger from 'redux-logger';
+import {createLogger} from 'redux-logger';
 
 const reduxActionsLogger = createLogger({
   collapsed: true,
   logErrors: true,
-  titleFormatter: ((action, time, took) => (`Action: ${action.type} [${time} ${Math.round(took)}ms]`)),
+  titleFormatter: ((action, time, took) => (`Action: %c${String(action.type)}%c [${time} ${Math.round(took)}ms]`)),
   stateTransformer: (state) => {
     const newState = {};
     Object.keys(state).forEach((i) => {
