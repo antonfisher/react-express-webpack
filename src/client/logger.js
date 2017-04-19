17a5f7e4e6fc09bc9424967117da5b8d1fcea736
Anton Fisher <a.fschr@gmail.com>
2017-04-18 22:01:34 -0700

    fix: lint errors

--- src/client/logger.js
+++ src/client/logger.js
@@ -4,7 +4,7 @@ import {createLogger} from 'redux-logger';
 const reduxActionsLogger = createLogger({
   collapsed: true,
   logErrors: true,
-  //titleFormatter: ((action, time, took) => (`Action: ${String(action.type)} [${time} ${Math.round(took)}ms]`)),
+  // titleFormatter: ((action, time, took) => (`Action: ${String(action.type)} [${time} ${Math.round(took)}ms]`)),
   stateTransformer: (state) => {
     const newState = {};
     Object.keys(state).forEach((i) => {
