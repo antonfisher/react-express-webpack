32c94ce094c8f132d6012ae2db4d231ac368c10f
Anton Fisher <a.fschr@gmail.com>
2017-06-19 22:33:10 -0700

    fix: properties priority

--- src/client/containers/ModalsLayout/reducer.js
+++ src/client/containers/ModalsLayout/reducer.js
@@ -7,9 +7,10 @@ const initialState = OrderedMap({});
 export default function ModalsLayoutReducer(state = initialState, action) {
   switch (action.type) {
     case SHOW_MODAL:
-      return state.set(action.payload.key, {open: true, ...action.payload.props});
+      return state.set(action.payload.key, {...action.payload.props, open: true});
     case ANIMATED_REMOVE_MODAL:
-      return state.set(action.payload.key, {open: false, ...action.payload.props});
+      const initialProps = state.get(action.payload.key);
+      return state.set(action.payload.key, {...initialProps, ...action.payload.props, open: false});
     case REMOVE_MODAL:
       return state.delete(action.payload.key);
     default:
