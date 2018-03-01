336e8263870388c3f284d898c095670f9ee179e6
Anton Fisher <a.fschr@gmail.com>
2018-02-28 23:14:14 -0800

    chore: update deps, fix lint

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,5 +1,4 @@
 const {resolve} = require('path');
-const webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
